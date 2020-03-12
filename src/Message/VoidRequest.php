cb8f022c0ea6edd2db655a200c11168bef147540
houseoftech <corie@houseoftech.com>
2020-03-12 18:11:33 +0100

    Add refund and void requests
    Also adds a login request to get sessionid which is required for certain requests and adds a transaction details request for determining refund type.

---
+++ src/Message/VoidRequest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Omnipay\PaymentVision\Message;
+
+use Omnipay\Common\Message\AbstractRequest;
+
+/**
+ * Send a request for voiding a transaction
+ */
+class VoidRequest extends AbstractRequest
+{
+    /**
+     * SoapClient Class
+     */
+    public $soap = null;
+
+    public function getSessionId()
+    {
+        return $this->getParameter('sessionId');
+    }
+
+    public function setSessionId($value)
+    {
+        return $this->setParameter('sessionId', $value);
+    }
+
+    public function getExternalRequestId()
+    {
+        return $this->getParameter('externalRequestId');
+    }
+
+    public function setExternalRequestId($value)
+    {
+        return $this->setParameter('externalRequestId', $value);
+    }
+
+    public function getRefundReason()
+    {
+        return $this->getParameter('refundReason');
+    }
+
+    public function setRefundReason($value)
+    {
+        return $this->setParameter('refundReason', $value);
+    }
+
+    public function getLiveWsdl()
+    {
+        return $this->getParameter('liveWsdl');
+    }
+
+    public function setLiveWsdl($value)
+    {
+        return $this->setParameter('liveWsdl', $value);
+    }
+
+    public function getTestWsdl()
+    {
+        return $this->getParameter('testWsdl');
+    }
+
+    public function setTestWsdl($value)
+    {
+        return $this->setParameter('testWsdl', $value);
+    }
+
+    public function getWsdl()
+    {
+        return $this->getTestMode() ? $this->getTestWsdl() : $this->getLiveWsdl();
+    }
+
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        $data = array();
+
+        $data['sessionID'] = $this->getSessionId();
+        $data['externalRequestID'] = $this->getExternalRequestId();
+        $data['reason'] = $this->getRefundReason();
+
+        return $data;
+    }
+
+    /**
+     * Send the request with specified data
+     *
+     * @param  mixed $data The data to send
+     * @return ResponseInterface
+     */
+    public function sendData($data)
+    {
+        if (!$this->soap) {
+            $this->soap = new \SoapClient($this->getWsdl());
+        }
+        
+        $response = call_user_func_array(array($this->soap, 'VoidAPIPaymentRequest'), array($data));
+
+        return $this->response = new Response($this, $response);
+    }
+}
