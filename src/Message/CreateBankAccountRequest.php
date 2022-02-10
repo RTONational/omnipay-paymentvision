4b9e068577d0b5d1d946b0e98cfe81ee037ef568
Mitchell Mahoney <mahoneymitch@gmail.com>
2022-02-10 15:53:33 -0500

    Implement fake response for CreateBankAccountRequest

--- src/Message/CreateBankAccountRequest.php
+++ src/Message/CreateBankAccountRequest.php
@@ -34,32 +34,32 @@ class CreateBankAccountRequest extends AbstractRequest
         $data['authentication'] = $this->getAuthenticationParams();
 
         $data['bankAccount'] = array(
-			'AccountType' => $this->getType(),
-			'ABA' => $this->getRoutingNumber(),
-			'AccountNumber' => $this->getAccountNumber(),
-			'BillingAddress' => array(
-				'NameOnAccount' => $this->getNameOnAccount(),
-				'AddressLineOne' => $this->getBillingAddress1(),
-				'City' => $this->getBillingCity(),
-				'State' => $this->getBillingState(),
-				'Zip' => substr($this->getBillingPostcode(), 0, 5),
-				'Phone' => preg_replace("/[^0-9]/", '', $this->getBillingPhone()),
-			),
+            'AccountType' => $this->getType(),
+            'ABA' => $this->getRoutingNumber(),
+            'AccountNumber' => $this->getAccountNumber(),
+            'BillingAddress' => array(
+                'NameOnAccount' => $this->getNameOnAccount(),
+                'AddressLineOne' => $this->getBillingAddress1(),
+                'City' => $this->getBillingCity(),
+                'State' => $this->getBillingState(),
+                'Zip' => substr($this->getBillingPostcode(), 0, 5),
+                'Phone' => preg_replace("/[^0-9]/", '', $this->getBillingPhone()),
+            ),
             'AccountUsePreferenceType' => 'MultiUse',
             'CheckMICROption' => array(
                 'CheckNumberPositionType' => 'RightOfAccount',
                 'CheckNumberAuxiliaryPositionType' => 'PrependToCheckNumber',
                 'CheckStockType' => 'Business',
             ),
-		);
-		
-		$data['customer'] = array(
-			'FirstName' => $this->getFirstName(),
-			'LastName' => $this->getLastName(),
-			'AdressLineOne' => $this->getBillingAddress1(),
-			'City' => $this->getBillingCity(),
-			'State' => $this->getBillingState(),
-			'Zip' => substr($this->getBillingPostcode(), 0, 5),
+        );
+
+        $data['customer'] = array(
+            'FirstName' => $this->getFirstName(),
+            'LastName' => $this->getLastName(),
+            'AdressLineOne' => $this->getBillingAddress1(),
+            'City' => $this->getBillingCity(),
+            'State' => $this->getBillingState(),
+            'Zip' => substr($this->getBillingPostcode(), 0, 5),
             'CustomerReferenceCode' => $this->getCustomerReferenceCode()
         );
 
@@ -89,12 +89,55 @@ class CreateBankAccountRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'AddBankAccount'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        $dateString = date('YmdHis');
+        return (object) [
+            'AddBankAccountResult' => [
+                'ReferenceID' => 'C' . $dateString,
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'CustomerReferenceCode' => 'CU' . $dateString,
+                'Bank' => [
+                    'ABA' => $data['bankAccount']['ABA'],
+                    'AccountNumber' => $data['bankAccount']['AccountNumber'],
+                    'AccountType' => $data['bankAccount']['AccountType'],
+                    'AccountOwnerType' => null,
+                    'BillingAddress' => $data['bankAccount']['BillingAddress'],
+                    'CustomBank' => [
+                        'BankName' => null,
+                        'BankCity' => null,
+                        'BankState' => null
+                    ],
+                    'CheckMICROption' => $data['bankAccount']['CheckMICROption'],
+                    'AccountUsePreferenceType' => $data['bankAccount']['AccountUsePreferenceType']
+                ],
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => ''
+                    ]
+                ]
+            ]
+        ];
     }
 }
