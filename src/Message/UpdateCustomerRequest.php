c263eba9551c6571abb9a3a4d2b87dc7623cb6c3
houseoftech <corie@houseoftech.com>
2022-02-11 19:03:11 +0100

    Implement FakeResponse for UpdateCustomerRequest

--- src/Message/UpdateCustomerRequest.php
+++ src/Message/UpdateCustomerRequest.php
@@ -56,12 +56,45 @@ class UpdateCustomerRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'UpdateCustomerInformation'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        return (object) [
+            'UpdateCustomerInformationResult' => [
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'Customer' => [
+                    'CustomerReferenceCode' => $data['customerReferenceCode'],
+                    'FirstName' => $data['customer']['FirstName'],
+                    'LastName' => $data['customer']['LastName'],
+                    'AdressLineOne' => $data['customer']['AdressLineOne'],
+                    'AddressLineTwo' => '',
+                    'City' => $data['customer']['City'],
+                    'State' => $data['customer']['State'],
+                    'Zip' => $data['customer']['Zip'],
+                    'HomePhone' => '               ',
+                    'WorkPhone' => '               ',
+                    'Email' => ''
+                ]
+            ]
+        ];
     }
 }
