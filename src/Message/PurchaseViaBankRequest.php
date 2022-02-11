80ee3b9c5be39daca4e78d41143ff76453cd34c1
Mitchell Mahoney <mahoneymitch@gmail.com>
2022-02-11 15:59:38 -0500

    Implement FakeResponse for PurchaseViaBankRequest

--- src/Message/PurchaseViaBankRequest.php
+++ src/Message/PurchaseViaBankRequest.php
@@ -76,7 +76,7 @@ class PurchaseViaBankRequest extends AbstractRequest
         if ($settlementDate = $this->getSettlementDate()) {
 			$data['achPayment']['SettlementDate'] = date('m/d/Y', strtotime($settlementDate));
 		}
-        
+
         $data['customer'] = array(
             'FirstName' => $this->getFirstName(),
             'LastName' => $this->getLastName(),
@@ -112,12 +112,56 @@ class PurchaseViaBankRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
         $response = call_user_func_array(array($this->soap, 'MakeACHPayment'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        $dateString = date('YmdHis');
+        return (object) [
+            'MakeACHPaymentResult' => [
+                'ReferenceID' => 'B' . $dateString,
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'TransactionStatus' => 'Pending',
+                'CaptureTrackingNumber' => 'API' . $dateString,
+                'TransactionReferenceCode' => 'STUB' . $dateString,
+                'CustomerReferenceCode' => 'CU' . $dateString,
+                'BankAccount' => [
+                    'ABA' => $data['bankAccount']['ABA'],
+                    'AccountNumber' => $data['bankAccount']['AccountNumber'],
+                    'AccountType' => $data['bankAccount']['AccountType'],
+                    'AccountOwnerType' => '',
+                    'BillingAddress' => $data['bankAccount']['BillingAddress'],
+                    'CustomBank' => '',
+                    'CheckMICROption' => $data['bankAccount']['CheckMICROption'],
+                    'AccountUsePreferenceType' => $data['bankAccount']['AccountUsePreferenceType'],
+                ],
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => '',
+                    ]
+                ],
+                'Amount' => $data['achPayment']['Amount'],
+                'PrincipalAmount' => $data['achPayment']['Amount'],
+                'ConvenienceFee' => '0.00',
+            ]
+        ];
     }
 }
