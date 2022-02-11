c077acca161bd30525c8a96d96518ed3b67d0853
Mitchell Mahoney <mahoneymitch@gmail.com>
2022-02-11 15:59:25 -0500

    Implement FakeResponse for LoginRequest

--- src/Message/LoginRequest.php
+++ src/Message/LoginRequest.php
@@ -60,12 +60,40 @@ class LoginRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'Login'), array($data));
 
         return new Response($this, $response);
+    }
+
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        $uuid = vsprintf('STUB%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(14)), 4));
+        return (object) [
+            'LoginResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => ''
+                    ]
+                ],
+                'SessionID' => $uuid
+            ]
+        ];
     }
 }
