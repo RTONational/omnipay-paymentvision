43742358f353e87c8198e64e5a6d70c3e367c001
houseoftech <corie@houseoftech.com>
2022-02-11 19:02:56 +0100

    Implement FakeResponse for UpdateBankAccountRequest

--- src/Message/UpdateBankAccountRequest.php
+++ src/Message/UpdateBankAccountRequest.php
@@ -36,14 +36,14 @@ class UpdateBankAccountRequest extends AbstractRequest
         $data['referenceID'] = $this->getReferenceId();
 
         $data['bankAccountUpdates'] = array(
-			'AccountType' => $this->getType(),
-			'BillingAddress' => array(
-				'NameOnAccount' => $this->getNameOnAccount(),
-				'AddressLineOne' => $this->getBillingAddress1(),
-				'City' => $this->getBillingCity(),
-				'State' => $this->getBillingState(),
-				'Zip' => substr($this->getBillingPostcode(), 0, 5),
-				'Phone' => preg_replace("/[^0-9]/", '', $this->getBillingPhone()),
+            'AccountType' => $this->getType(),
+            'BillingAddress' => array(
+                'NameOnAccount' => $this->getNameOnAccount(),
+                'AddressLineOne' => $this->getBillingAddress1(),
+                'City' => $this->getBillingCity(),
+                'State' => $this->getBillingState(),
+                'Zip' => substr($this->getBillingPostcode(), 0, 5),
+                'Phone' => preg_replace("/[^0-9]/", '', $this->getBillingPhone()),
                 'CustomerReferenceCode' => $this->getCustomerReferenceCode()
             ),
             'Customer' => array(
@@ -70,12 +70,70 @@ class UpdateBankAccountRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'UpdateBankAccount'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        return (object) [
+            'UpdateBankAccountResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => ''
+                    ]
+                ],
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'BankAccountToken' => [
+                    'ReferenceID' => $data['referenceID'],
+                    'BankAccount' => [
+                        'ABA' => '122199983',
+                        'AccountNumber' => '12',
+                        'AccountType' => 'Checking',
+                        'AccountOwnerType' => '',
+                        'BillingAddress' => [
+                            'NameOnAccount' => $data['bankAccountUpdates']['BillingAddress']['NameOnAccount'],
+                            'AddressLineOne' => $data['bankAccountUpdates']['BillingAddress']['AddressLineOne'],
+                            'City' => $data['bankAccountUpdates']['BillingAddress']['City'],
+                            'State' => $data['bankAccountUpdates']['BillingAddress']['State'],
+                            'Zip' => $data['bankAccountUpdates']['BillingAddress']['Zip'],
+                            'Phone' => $data['bankAccountUpdates']['BillingAddress']['Phone']
+                        ],
+                        'CustomBank' => [
+                            'BankName' => '',
+                            'BankCity' => '',
+                            'BankState' => ''
+                        ],
+                        'CheckMICROption' => [
+                            'CheckNumberPositionType' => '',
+                            'CheckNumberAuxiliaryPositionType' => '',
+                            'CheckStockType' => ''
+                        ],
+                        'AccountUsePreferenceType' => 'MultiUse'
+                    ],
+                    'FinancialAccountStatusType' => 'Active',
+                    'LastUsed' => '0001-01-01T00:00:00',
+                    'LastUpdated' => '0001-01-01T00:00:00',
+                    'Created' => '0001-01-01T00:00:00'
+                ]
+            ]
+        ];
     }
 }
