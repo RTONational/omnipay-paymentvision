175e682aa58c909ff24cb6b80f68bd162621e1cd
houseoftech <corie@houseoftech.com>
2022-02-11 19:03:17 +0100

    Implement FakeResponse for VoidRequest

--- src/Message/VoidRequest.php
+++ src/Message/VoidRequest.php
@@ -47,12 +47,92 @@ class VoidRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'VoidAPIPaymentRequest'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        $dateString = date('YmdHis');
+        return (object) [
+            'VoidAPIPaymentRequestResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => '',
+                    ],
+                ],
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'RecordCount' => '1',
+                'TransactionResponses' => [
+                    'anyType' => [
+                        '@attributes' => [
+                            'xsi:type' => 'CreditCardPaymentResponse',
+                        ],
+                        'ReferenceID' => 'C' . $dateString,
+                        'TimeReceived' => date('n/j/Y g:i:s A'),
+                        'TransactionStatus' => 'Cancelled',
+                        'CaptureTrackingNumber' => 'API' . $dateString,
+                        'TransactionReferenceCode' => 'STUB' . $dateString,
+                        'CustomerReferenceCode' => 'CU' . $dateString,
+                        'Amount' => '100.0000',
+                        'PrincipalAmount' => '100.0000',
+                        'ConvenienceFee' => '0.0000',
+                        'CreditCardAccount' => [
+                            'CreditCardNumber' => 'XXXXXXXXXXXX1111',
+                            'CreditCardExpirationMonth' => '12',
+                            'CreditCardExpirationYear' => date('Y', strtotime('+1 year')),
+                            'CardType' => 'Visa',
+                            'BillingAddress' => [
+                                'NameOnCard' => 'Testy Tester',
+                                'AddressLineOne' => '1502 N Main St.',
+                                'AddressLineTwo' => [],
+                                'City' => 'Greenville',
+                                'State' => 'SC',
+                                'ZipCode' => '29609',
+                                'Phone' => [],
+                            ],
+                            'FulfillmentGateway' => [
+                                '@attributes' => [
+                                    'xsi:nil' => 'true',
+                                ],
+                            ],
+                            'AccountUsePreferenceType' => [
+                                '@attributes' => [
+                                    'xsi:nil' => 'true',
+                                ],
+                            ],
+                            'CreditCardBinType' => [
+                                '@attributes' => [
+                                    'xsi:nil' => 'true',
+                                ],
+                            ],
+                        ],
+                        'Responses' => [
+                            'Response' => [
+                                'ResponseCode' => '1000',
+                                'ErrorMessage' => '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
     }
 }
