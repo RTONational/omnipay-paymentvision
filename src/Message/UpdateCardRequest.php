989168be7f22c0feb31ab8d8d3db3dcfb61af85a
houseoftech <corie@houseoftech.com>
2022-02-11 19:03:03 +0100

    Implement FakeResponse for UpdateCardRequest

--- src/Message/UpdateCardRequest.php
+++ src/Message/UpdateCardRequest.php
@@ -39,14 +39,14 @@ class UpdateCardRequest extends AbstractRequest
 
         $data['creditCardAccountUpdates'] = array(
             'BillingAddress' => array(
-				'NameOnCard' => $this->getNameOnCard(),
-				'AddressLineOne' => $card->getBillingAddress1(),
-				'City' => $card->getBillingCity(),
+                'NameOnCard' => $this->getNameOnCard(),
+                'AddressLineOne' => $card->getBillingAddress1(),
+                'City' => $card->getBillingCity(),
                 'State' => $card->getBillingState(),
                 'ZipCode' => substr($card->getBillingPostcode(), 0, 5),
                 'Phone' => preg_replace("/[^0-9]/", '', $card->getBillingPhone()),
                 'CustomerReferenceCode' => $this->getCustomerReferenceCode(),
-			),
+            ),
             'Customer' => array(
                 'FirstName' => $card->getFirstName(),
                 'LastName' => $card->getLastName(),
@@ -69,12 +69,62 @@ class UpdateCardRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'UpdateCreditCardAccount'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        return (object) [
+            'UpdateCreditCardAccountResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => ''
+                    ]
+                ],
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'CreditCardAccountToken' => [
+                    'ReferenceID' => $data['referenceID'],
+                    'CreditCardAccount' => [
+                        'CreditCardNumber' => 'XXXXXXXXXXXX1111',
+                        'CreditCardExpirationMonth' => '12',
+                        'CreditCardExpirationYear' => date('Y', strtotime('+1 year')),
+                        'CardType' => 'Visa',
+                        'BillingAddress' => [
+                            'NameOnAccount' => $data['creditCardAccountUpdates']['BillingAddress']['NameOnAccount'],
+                            'AddressLineOne' => $data['creditCardAccountUpdates']['BillingAddress']['AddressLineOne'],
+                            'City' => $data['creditCardAccountUpdates']['BillingAddress']['City'],
+                            'State' => $data['creditCardAccountUpdates']['BillingAddress']['State'],
+                            'Zip' => $data['creditCardAccountUpdates']['BillingAddress']['Zip'],
+                            'Phone' => $data['creditCardAccountUpdates']['BillingAddress']['Phone']
+                        ],
+                        'FulfillmentGateway' => '',
+                        'AccountUsePreferenceType' => 'MultiUse',
+                        'CreditCardBinType' => ''
+                    ],
+                    'FinancialAccountStatusType' => 'Active',
+                    'LastUsed' => '0001-01-01T00:00:00',
+                    'LastUpdated' => '0001-01-01T00:00:00',
+                    'Created' => '0001-01-01T00:00:00'
+                ]
+            ]
+        ];
     }
 }
