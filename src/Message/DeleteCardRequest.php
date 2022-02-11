06f6dd5f5e84695873470a491f38cea0a042dc6d
Mitchell Mahoney <mahoneymitch@gmail.com>
2022-02-11 15:59:15 -0500

    Implement FakeResponse for DeleteCardRequest

--- src/Message/DeleteCardRequest.php
+++ src/Message/DeleteCardRequest.php
@@ -46,12 +46,66 @@ class DeleteCardRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'UnregisterCreditCardAccount'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        $dateString = date('YmdHis');
+        return (object) [
+            'UnregisterCreditCardAccountResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => '',
+                    ]
+                ],
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+                'CustomerReferenceCode' => 'CU' . $dateString,
+                'CreditCardAccountToken' => [
+                    'ReferenceID' => $data['referenceID'],
+                    'CreditCardAccount' => [
+                        'CreditCardNumber' => 'XXXXXXXXXXXX1111',
+                        'CreditCardExpirationMonth' => '12',
+                        'CreditCardExpirationYear' => '2023',
+                        'CardType' => 'Visa',
+                        'BillingAddress' => [
+                            'NameOnCard' => 'Testy Tester',
+                            'AddressLineOne' => '123 Main Street',
+                            'AddressLineTwo' => '',
+                            'City' => 'Greenville',
+                            'State' => 'SC',
+                            'ZipCode' => '29605',
+                            'Phone' => '',
+                        ],
+                        'FulfillmentGateway' => '',
+                        'AccountUsePreferenceType' => 'MultiUse',
+                        'CreditCardBinType' => '',
+                    ],
+
+                    'FinancialAccountStatusType' => 'Active',
+                    'LastUsed' => '0001-01-01T00:00:00',
+                    'LastUpdated' => '0001-01-01T00:00:00',
+                    'Created' => '0001-01-01T00:00:00',
+                ]
+            ]
+        ];
     }
 }
