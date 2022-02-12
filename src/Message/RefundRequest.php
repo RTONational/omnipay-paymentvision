ff33a8b25445902088fa9c9f54ea81a6bbfeacd3
houseoftech <corie@houseoftech.com>
2022-02-12 11:07:53 +0100

    Implement FakeResponse for RefundRequest
    In so far as what is known about the response.

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -48,12 +48,38 @@ class RefundRequest extends AbstractRequest
      */
     public function sendData($data)
     {
+        if (true === $this->getStubMode()) {
+            $response = $this->getFakeResponse($data);
+            return $this->response = new FakeResponse($this, $response);
+        }
+
         if (!$this->soap) {
             $this->soap = new \SoapClient($this->getWsdl(), array('trace' => $this->getTestMode()));
         }
-        
+
         $response = call_user_func_array(array($this->soap, 'RefundTransaction'), array($data));
 
         return $this->response = new Response($this, $response);
+    }
+
+    /**
+     * Get fake response specific to this request
+     *
+     * @param  mixed $data The data that would otherwise be sent
+     * @return object
+     */
+    public function getFakeResponse($data)
+    {
+        return (object) [
+            'RefundTransactionResult' => [
+                'Responses' => [
+                    'Response' => [
+                        'ResponseCode' => '1000',
+                        'ErrorMessage' => ''
+                    ]
+                ],
+                'TimeReceived' => date('n/j/Y g:i:s A'),
+            ]
+        ];
     }
 }
